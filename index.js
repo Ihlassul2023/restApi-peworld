fbe10d989f2406c4c8493e19a4088b512c0e74b3
Mahardhika Pratama <putrad578@gmail.com>
2023-08-17 09:57:48 +0700

    import register user at index.js and use that

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const registerUser = require('./src/router/authRouter')
 
 
 app.use(express.json());
@@ -8,6 +9,8 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
     res.json({ info: 'HireJob API v.1.0.0' })
 })
+
+app.use(registerUser)
 
 app.listen(4000, ()=>{
     console.log(`App running on http://localhost:4000`)
