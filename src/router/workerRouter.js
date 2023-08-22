180b78c14ebc485b8202d0d5cd7831ef118ec7ae
Ihlassul2023 <ihlascamp@gmail.com>
2023-08-23 01:31:47 +0800

    fix all feature

--- src/router/workerRouter.js
+++ src/router/workerRouter.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
-const { registerWorker, getProfileWorker, getWorker, loginWorker, editWorker, getById, deleteAccount } = require("../controller/workerController");
-
-=======
 const { registerWorker, getWorker, loginWorker, editWorker, getById, getProfileWorker, deleteAccount, verify } = require("../controller/workerController");
->>>>>>> menu/profile
+
 const app = require("express");
 const router = app.Router();
 const upload = require("../middleware/multer");
@@ -15,11 +11,7 @@ router.get("/list-worker/:id", getById);
 router.post("/register-worker", upload.single("photo"), registerWorker);
 router.post("/login-worker", loginWorker);
 router.put("/update-worker", protect, upload.single("photo"), editWorker);
-<<<<<<< HEAD
 router.delete("/delete-worker", protect, deleteAccount);
-=======
-router.delete("/delete-worker", protect, deleteAccount)
-router.get('/verify-worker/:id', verify)
->>>>>>> menu/profile
+router.get("/verify-worker/:id", verify);
 
 module.exports = router;
